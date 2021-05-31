15356d6201e630a86253fe392768d5b66f61403b
Deepika Shrestha <sthadpka93@gmail.com>
2021-05-31 11:24:59 +0000

    Upload New File

---
+++ query.py
@@ -0,0 +1,63 @@
+from flask import Flask, Response, json
+from flask import jsonify, request
+from flask_pymongo import PyMongo
+from flask_cors import CORS
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
+
+app = Flask(__name__)
+
+try:
+
+    app.secret_key = "secretkey"
+
+    app.config['MONGO_URI'] = "mongodb+srv://Hotel:hotelms@hotelms.ueddl.mongodb.net/Hotel?retryWrites=true&w=majority"
+
+    mongo = PyMongo(app)
+
+except Exception as e:
+    print(e)
+    print('***************couldnot conncect to db************')
+
+
+@app.route('/')
+def index():
+    return "HEYYYYYYYYYYYY"
+
+@app.route('/room/available', methods=["GET"])
+def available_room():
+    
+    search= list(mongo.db.room.find({"room_status": "vacant"}))
+
+    for information in search:
+        information["_id"]= str(information["_id"])
+    return Response( 
+        response = json.dumps(search),
+        status=200,
+        mimetype="application/json"
+                             )
+
+
+@app.route('/customer/<id>', methods=["GET"])
+def customer(id):
+    
+
+    customers= list(mongo.db.customer.aggregate([{"$match":{'_id': ObjectId(id)}}, {"$lookup":{ "from" :"room", "localField": "booking_info.room_id","foreignField":"_id","as":"details" }}]))
+    print(id)
+
+    for attributes in customers:
+        attributes["_id"] = str(attributes["_id"])
+
+        return Response(
+        response =json.dumps(str(customers)),
+        status=200,
+        mimetype="application/json" )
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
