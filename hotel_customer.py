f52f3e254fd4b34e72ebfb277caf0e793e8a532e
pranavshrestha11 <pranavshrestha11@gmail.com>
2021-05-31 12:49:45 +0545

    modified hotel_customer

--- hotel_customer.py
+++ hotel_customer.py
@@ -6,6 +6,8 @@ from flask_cors import CORS
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -28,17 +30,19 @@ def add_customer():
     _age = _json["age"]
     _phone = _json["phone"]
     _address = _json["address"]
-    _room_no = _json["booking_info"]["room_no"]
+    _room_id = _json["booking_info"]["room_id"]
     _check_in = _json["booking_info"]["check_in"]
+    _check_in = datetime.strptime(_check_in, "%Y-%m-%d %H:%M:%S")
     _check_out = _json["booking_info"]["check_out"]
+    _check_out = datetime.strptime(_check_out, "%Y-%m-%d %H:%M:%S")
     _total_cost = _json["booking_info"]["total_cost"]
     _payment_method = _json["booking_info"]["payment_method"]
 
-    if _customer_name and _age and _phone and _address and _room_no and _check_in and _check_out and _total_cost and _payment_method and request.method == 'POST':
+    if _customer_name and _age and _phone and _address and _room_id and _check_in and _check_out and _total_cost and _payment_method and request.method == 'POST':
 
         id = mongo.db.customer.insert({'customer_name': _customer_name, 'age': _age,
                 'phone': _phone, 'address': _address, 'booking_info': 
-                {'room_no': _room_no, 'check_in': _check_in, 'check_out': _check_out,
+                {'room_id': ObjectId(_room_id), 'check_in': _check_in, 'check_out': _check_out,
                 'total_cost': _total_cost, 'payment_method': _payment_method}})
 
         resp = jsonify("Customer added successfully")
@@ -63,18 +67,20 @@ def update_customer(id):
     _age = _json["age"]
     _phone = _json["phone"]
     _address = _json["address"]
-    _room_no = _json["booking_info"]["room_no"]
+    _room_id = _json["booking_info"]["room_id"]
     _check_in = _json["booking_info"]["check_in"]
+    _check_in = datetime.strptime(_check_in, "%Y-%m-%d %H:%M:%S")
     _check_out = _json["booking_info"]["check_out"]
+    _check_out = datetime.strptime(_check_out, "%Y-%m-%d %H:%M:%S")
     _total_cost = _json["booking_info"]["total_cost"]
     _payment_method = _json["booking_info"]["payment_method"]
 
-    if _customer_name and _age and _phone and _address and _room_no and _check_in and _check_out and _total_cost and _payment_method and request.method == 'PUT':
+    if _customer_name and _age and _phone and _address and _room_id and _check_in and _check_out and _total_cost and _payment_method and request.method == 'PUT':
         
         mongo.db.customer.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, 
                 {'$set': {'customer_name': _customer_name, 'age': _age,
                 'phone': _phone, 'address': _address, 'booking_info': 
-                {'room_no': _room_no, 'check_in': _check_in, 'check_out': _check_out,
+                {'room_id': ObjectId(_room_id), 'check_in': _check_in, 'check_out': _check_out,
                 'total_cost': _total_cost, 'payment_method': _payment_method}}})
 
         resp = jsonify("Customer updated successfully")
@@ -101,6 +107,12 @@ def not_found(error=None):
     resp.status_code = 404
 
     return resp
+
+
+#convert datestr into date format
+def date_conversion(date_str):
+    date = datetime.strptime(date_str, "%Y-%m-%d")
+    return date
 
 if __name__ == '__main__':
     app.run(debug=True)
